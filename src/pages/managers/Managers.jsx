f4dca0ff8eaf93b582f47568001326e688625b2f
yeray004 <yeye20041@gmail.com>
2023-02-06 17:13:25 -0500

    Folder base structure

---
+++ src/pages/managers/Managers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Managers() {
+    return (
+        <div>Managers</div>
+    )
+}
+
+export default Managers
---
+++ src/pages/matrix/Matrix.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Matrix() {
+    return (
+        <div>Matrix</div>
+    )
+}
+
+export default Matrix
