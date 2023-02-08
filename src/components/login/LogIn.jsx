74d83a94e606f33c1f93820d3836330c3fb47d58
Amanda Forero <amandaforero@outlook.com>
2023-02-08 13:36:20 -0500

    Responsive and changing login styles

--- src/components/login/LogIn.jsx
+++ src/components/login/LogIn.jsx
@@ -1,4 +1,5 @@
 import "./login.css";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
@@ -6,25 +7,50 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Logo from "../../assets/img/logo.svg";
 import { BsFillPersonFill, BsFillLockFill } from "react-icons/bs";
+import axios from "axios";
 
 const LogIn = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState('');
+  const [loading, setloading] = useState(false);
+  const { email, password } = inputs;
+  const onChange = (e) => {
+    setInputs({...inputs,[e.target.email]:e.target.value})
+  }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    if(email !=='' && password !==''){
+      const User ={
+        email,
+        password
+      };
+      setloading(true)
+      await axios
+      .post('http://localhost:3030', User)
+      .then(({data}) => console.log(data))
+    }
+  };
   return (
     <>
-      <Container className="text-center">
-        <Row className="">
-          <Col className="logoContainer">
+      <div className="containerLogIn">
+          <div className="logoContainer">
             <img src={Logo} alt="Logo" />
-          </Col>
-          <Col className="form">
+          </div>
+          <div className="vr"></div>
+          <div className="form">
             <p className="log-in">Iniciar sesión</p>
             <hr />
-            <Form>
+            <Form className="inputsForm"onSubmit={(e) => (onSubmit(e))}>
               <Form.Group as={Row}>
                 <Form.Label column sm="1">
                   <BsFillPersonFill />
                 </Form.Label>
-                <Col sm="10">
+                <Col sm="11">
                   <Form.Control
+                    onChange={(e) => (onChange(e))}
                     type="email"
                     className="logInInput"
                     placeholder="Usuario"
@@ -33,31 +59,24 @@ const LogIn = () => {
               </Form.Group>
               <Form.Group
                 as={Row}
-                className="mb-3"
                 controlId="formBasicPassword"
               >
                 <Form.Label column sm="1">
                   <BsFillLockFill />
                 </Form.Label>
-                <Col sm="10">
+                <Col sm="11">
                   <Form.Control
+                    onChange={(e) => (onChange(e))}
                     type="password"
                     className="logInInput"
                     placeholder="Contraseña"
                   />
                 </Col>
               </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="formBasicCheckbox"
-              ></Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
+              <Button type="submit">Ingresar</Button>
             </Form>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+      </div>
     </>
   );
 };
