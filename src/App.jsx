843f7de1b3ef06a88a2186aabc50a43b5709c6c2
amazingdani <d.posada1044@gmail.com>
2023-02-08 12:04:56 -0500

    header style applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
-import Matrix from './pages/matrix/Matrix'
+import './components/header/Header.css'
+import Header from './components/header/Header.jsx'
 
 function App() {
   return (
     <>
-      <Matrix/>
+      <Header/>
     </>
   )
 }
---
+++ src/pages/managers/addManagers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function addManagers() {
+    return (
+        <div>addManagers</div>
+    )
+}
+
+export default addManagers
