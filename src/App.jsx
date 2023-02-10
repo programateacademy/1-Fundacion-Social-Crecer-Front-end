ac8f8e40d0a8f2995a5bb72f3f1642cbb1f9ec27
amazingdani <d.posada1044@gmail.com>
2023-02-10 09:35:24 -0500

    modal added with the needed information for create a new user

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from'./components/header/Header.jsx';
 import CardUser from './pages/managers/CardUser.jsx';
 import './pages/managers/Managers.css';
-import AddManagersButton from './pages/managers/AddManagersButton.jsx';
+//import AddManagersButton from './pages/managers/AddManagersButton.jsx';
 import SearchManagers from './pages/managers/SearchManagers.jsx';
+import ModalContainerAddUser from './pages/managers/ModalContainerAddUser.jsx';
 function App() {
   return (
     <>
       <Header />
       <div className='filaUno'>
       <SearchManagers/>
-      <AddManagersButton/>
+      <ModalContainerAddUser/>
       </div>
       <CardUser/>
     </>
