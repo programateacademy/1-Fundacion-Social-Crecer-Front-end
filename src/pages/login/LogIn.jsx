7c2c58bcaf38a52ac22cff96e33c6672294776c5
Amanda Forero <amandaforero@outlook.com>
2023-02-10 09:22:47 -0500

    Structuring routes in app.jsx

--- src/pages/login/LogIn.jsx
+++ src/pages/login/LogIn.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// import LogIn from "./components/login/LogIn.jsx";
+// import admin from "./apis";
 
-function LogIn() {
-    return (
-        <div>LogIn</div>
-    )
-}
+// function LogIn() {
+//   // Fetch data from backend
+//   const add = async (item) => {
+//     const { data } = await admin.post("/api/user/login", item);
+//     console.log(data);
+//   };
+//   return (
+//     <>
+//       <LogIn addFunction={add} />
+//     </>
+//   );
+// }
 
-export default LogIn
+// export default LogIn;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,11 +2,16 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import LogIn from "../components/login/LogIn";
 
 function App() {
+  // Fetch data from backend
+  const add = async (item) => {
+    const { data } = await admin.post("/api/user/login", item);
+    console.log(data);
+  };
   return (
     <>
       <HashRouter>
         <Routes>
-          <Route path="/" element={<LogIn />} />
+          <Route path="/" element={<LogIn addFunction={add} />} />
         </Routes>
       </HashRouter>
     </>
