fed66aa7c2ddef4d40d16658cba229b33a06a293
Amanda Forero <amandaforero@outlook.com>
2023-02-21 13:27:02 -0500

    Api connection changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL=  "http://localhost:3001/";
+const BASE_URL=   import.meta.env.VITE_URL_BACKEND || "https://funcrecer-back-admins.vercel.app/";
 
 const API = axios.create({
     baseURL:BASE_URL
