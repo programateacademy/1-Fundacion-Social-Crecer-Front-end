323c0bf9390fea372cb2fb9ab16b6b1d518b66f4
DruckDeveloper <druckdev@gmail.com>
2023-02-25 18:28:25 -0500

    adding button to send email

--- src/components/modals/lockedaccountmodals/AdminLockedModal.jsx
+++ src/components/modals/lockedaccountmodals/AdminLockedModal.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+import app from '../../../apis/index'
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import IconPadLock from "../../icons/IconPadLock";
 
 const AdminLockedModal = ( props ) => {
-  const superAdminMessagge =
-    "Se ha enviado un mensaje deseguridad a tu email registrado";
+  const superAdminMessagge = "Se ha enviado un mensaje deseguridad a tu email registrado";
   const adminMessagge = "Ponte en contacto con el encargado ";
 
+  const [Code, setCode] = useState()
+
+  const handleCodeVerification = async () => {
+    try {
+      const response = await app.get('/api/code', { headers: { Code: process.env.VITE_CODE_KEY }});
+      setCode(response.data.code);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   return (
     <Modal
       {...props}
@@ -23,7 +35,13 @@ const AdminLockedModal = ( props ) => {
         <IconPadLock />
         <div className="d-flex flex-column justify-content-between fs-4">
           <p>Has excedido la cantidad maxima de intentos para iniciar sesion</p>
-          <p>{props.role == "admin" ? adminMessagge : superAdminMessagge}</p>
+          <p> {props.role == "admin" ? adminMessagge : superAdminMessagge}</p>
+          <div>
+            {props.role == "superadmin" ? 
+            <Button variant="primary" className="w-50" size="sm" onClick={() => handleCodeVerification() }>
+              Enviame el codigo
+            </Button> : ''}
+          </div>
         </div>
       </Modal.Body>
     </Modal>
