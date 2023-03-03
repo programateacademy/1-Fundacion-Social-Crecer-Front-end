471195a4456e7bf4992a54bcfb1e47aebfebd658
DruckDeveloper <druckdev@gmail.com>
2023-03-02 22:22:07 -0500

    super admin password verification for the managers page

--- src/pages/managers/Managers.jsx
+++ src/pages/managers/Managers.jsx
@@ -25,7 +25,7 @@ function Managers({onLogout, token}) {
 
   useEffect(() => {
     getManagers();
-  }, [getManagers]);
+  }, []);
 
   let quitAccent=function (cadena){
     const acentos = {'á':'a','é':'e','í':'i','ó':'o','ú':'u','Á':'A','É':'E','Í':'I','Ó':'O','Ú':'U'};
