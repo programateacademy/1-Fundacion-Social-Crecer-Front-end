b090a0b77ab48dab0fff8fa399ed3a6679d8de8d
Amanda Forero <amandaforero@outlook.com>
2023-03-02 20:49:12 -0500

    selectOptions object used to check ternary of select on map in Tbody.jsx

--- src/pages/matrix/table/Tbody.jsx
+++ src/pages/matrix/table/Tbody.jsx
@@ -24,7 +24,53 @@ function Tbody({ token }) {
     const beneficiariesNameValues = Object.keys(dummy());
 
     const dateKeys = ["joinDate", "exitDate", "birthDate", "guardianBirthdate", "fatherBirthdate", "motherBirthdate", "vaccinationVerificationDate", "vaccinationCardUpToDate",];
-    const numberKeys = ["primaryPhone", "secundaryPhone", "householdStratum","gestationalAgeAtBirth", "weightAtBirth", "heightAtBirth", "exclusiveBreastfeedingDuration", "totalBreastfeedingDuration", "gestationWeeks", "ticketNumber"]
+    const numberKeys = ["primaryPhone", "secundaryPhone", "householdStratum", "gestationalAgeAtBirth", "weightAtBirth", "heightAtBirth", "exclusiveBreastfeedingDuration", "totalBreastfeedingDuration", "gestationWeeks", "ticketNumber"]
+    const selectOptions = {
+        curState: ['ACTIVO', 'INACTIVO'],
+        otherExitReason:['MOTIVO 1','MOTIVO 2'],
+        unityName:['EDUCANDO ANDO','SEMBRANDO ESPERANZA','ESPACIOS CREATIVOS','CRECER JUGANDO EN FAMILIA','CRECIENDO CON AMOR EN FAMILIA','MUNDO DE COLORES','LACITOS DE AMOR','MIS TERNURAS','CRECER Y CREAR 1','CRECER Y CREAR 2','EMPRENDEDORAS','MONACHOS','SEMILLITAS DE AMOR','SOÑADORAS','GUERRERAS CONSTRUYE','TRIUNFADORAS','GESTANDO FUTURO T3','GESTANDO FUTURO T2','GRAN BRITALIA T2','ABRAHAM LINCOLN T3','LOURDES','LA PEÑA','SANTA ROSA DE LIMA','CASA EGIPTO','SANTA BARBARA','FISCALIA','USME PUEBLO','SERRANIAS','VIRREY','SAN JUAN A','EL UVAL','TRIANGULO','LORENZO ALCANTUZ I SECTOR','M1 AMBA CHAKE','M2 GALAN','M3 EDUARDO SANTOS','M4 MUZUUN MUNDO MEJOR'],
+        duoName:['HILANDO CAMINOS','TIHUAQUE','U1-VINCULOS DE AMOR','U1-VINCULOS DE AMOR VEREDAS','U2-MIS ANGELITOS','U3-SEMILLAS DE PAZ','U4-TEJIENDO SABERES','U5-GRANDES TALENTOS'],
+        documentType:['RC','CC','TI','PEP','PASAPORTE','SIN DOCUMENTO','ANM','PPT'],
+        gender:['FEMENINO','MASCULINO'],
+        birthCountry:['COLOMBIA','VENEZUELA','ECUADOR','PERÚ','PANAMÁ','BRAZIL'],
+        disability:['NO','SI'],
+        certifiedDisability:['NO','SI'],
+        disabilityCategory:['NINGUNA','FÍSICA','INTELECTUAL','PSICOSOCIAL','AUDITIVA','VISUAL','SORDO SEGUERA','MULTIPLE','SENSORIAL','SISTEMICA','VOZ Y HABLA','PIEL, PELO Y UÑAS'],
+        disabilityRegistryEnrollment:['NO','SI'],
+        requiresAssistance:['NO','SI'],
+        requiresTechSupport:['NO','SI'],
+        hasTechSupport:['NO','SI'],
+        requiresTherapy:['NO','SI'],
+        receivesTherapy:['NO','SI'],
+        hasInterdictionProcess:['NO','SI'],
+        countryOfResidence:['COLOMBIA'],
+        residenceDepartment:['BOGOTÁ, D.C.'],
+        locationZone:['CABECERA','RESTO'],
+        headerType:['LOCALIDAD','COMUNA','NO APLICA'],
+        householdStratum:['0','1','2','3'],
+        groupEthnicity:['NO SE AUTORECONOCE EN NINGUNO DE LOS ANTERIORES','AFROCOLOMBIANO','INDÍGENA','RAIZAL DEL ARCHIPIELAGO DE SAN ANDRES, PROVIDENCIA Y SANTA CATALINA'],
+        beneficiarySisbenized:['NO','SI'],
+        sisbenScore:['A1','A2','A3','A4','A5','B1','B2','B3','B4','B5','B6','B7','C1','C2','C3','C4','C5','C6','C7'],
+        belongsToFamiliesInAction:['NO','SI'],
+        directlyAffectedByArmedConflict:['NO','SI'],
+        focusingCriteria:['A','B','C','D','E','F','H','I','J','K','L','M','N','O'],
+        justificationDocumentExists:['NO','SI'],
+        guardianPersonType:['MADRE','PADRE','TÍO(A)','HERMANO(A)','ABUELO(A)','PADRASTRO','MADRASTRA','CONYUGUE','AMIGO(A)','OTRO'],
+        guardianDocumentType:['CC','TI','PEP','PASAPORTE','SIN DOCUMENTO'],
+        guardianBirthCountry:['COLOMBIA','VENEZUELA','ECUADOR','PERÚ','PANAMÁ','BRAZIL'],
+        fatherDocumentType:['CC','TI','PEP','PASAPORTE','SIN DOCUMENTO'],
+        fatherBirthCountry:['COLOMBIA','VENEZUELA','ECUADOR','PERÚ','PANAMÁ','BRAZIL'],
+        motherDocumentType:['CC','TI','PEP','PASAPORTE','SIN DOCUMENTO'],
+        motherBirthCountry:['COLOMBIA','VENEZUELA','ECUADOR','PERÚ','PANAMÁ','BRAZIL'],
+        regime:['NO SE ENCUENTRA AFILIADO','SUBSIDIADO','CONTRIBUTIVO','ESPECIAL'],
+        eps: ['NO SE ENCUENTRA AFILIADO','AIC-EPSI','ALIANSALUD EPS','AMBUQ EPS','ANAS WAYUU','ASMET SALUD EPS','CAJACOPI EPS','CAPITAL SALUD EPS','CAPRESOCA EPS','COMFACHOCÓ EPS','COMFACOR','COMFAGUAJIRA','COMFAMILIAR CARTAGENA','COMFAMILIAR EPS','COMFAMILIAR NARIÑO','COMPARTA EPS','COMPENSAR','CONFAORIENTE EPS','CONFASUCRE','CONVIDA EPS','COOSALUD','DUASAKAWI EPSI','ECOOPSOS','EMSSANAR EPS','EPS SANITAS','FAMISANAR EPS','MALLAMAS EPS INDÍGENA','MEDIMÁS EPS','MUTUAL SER','NUEVA EPS','PIJAOS SALUD','SALUD TOTAL EPS','SAVIA SALUD EPS','SURA EPS'],
+        hasVaccinationCard:['NO','SI'],
+        vaccinationCardUpToDate:['NO','SI'],
+        hasGrowthAndDevelopmentCard:['NO','SI'],
+        prematurenessBackground: ['NO','SI'],
+        under40Weeks: ['NA','NO','SI'],
+        exclusivelyBreastfeeding: ['NO','SI']
+    }
     const updateBeneficiary = async (id, beneficiary) => {
         const url = `/api/admin/beneficiary/${id}`;
         const config = {
@@ -74,7 +120,7 @@ function Tbody({ token }) {
     return (
         <>
 
-            <tbody striped bordered hover>
+            <tbody>
                 {array.map((beneficiary) => (
                     // numDoc identificador unico
                     // Key identificador de filas
@@ -93,24 +139,40 @@ function Tbody({ token }) {
                         </td>
                         {/* Array de las propiedades de los beneficiarios */}
                         {beneficiariesNameValues.map((item) => (
-                            // Key identificador de columnas
                             <td key={item}>
                                 {isEditing && editedItem._id === beneficiary._id ? (
-                                    <input
-                                        name={item}
-                                        value={editedItem[item]}
-                                        type={dateKeys.includes(item) ? "date" : "text" && numberKeys.includes(item)? "number" : "text"}
-                                        placeholder={item}
-                                        onChange={(e) => {
-                                            const newItem = editedItem;
-                                            newItem[item] = e.target.value
-                                            setEditedItem(newItem);
-                                            console.log(editedItem);
-                                            onChangeInput(e, editedItem._id)
-                                        }}
-
-
-                                    />
+                                    // Verificar si el elemento actual es un select
+                                    selectOptions[item] ? (
+                                        <select
+                                            name={item}
+                                            value={editedItem[item]}
+                                            onChange={(e) => {
+                                                const newItem = editedItem;
+                                                newItem[item] = e.target.value;
+                                                setEditedItem(newItem);
+                                                onChangeInput(e, editedItem._id);
+                                            }}
+                                        >
+                                            {selectOptions[item].map((option) => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    ) : (
+                                        <input
+                                            name={item}
+                                            value={editedItem[item]}
+                                            type={dateKeys.includes(item) ? "date" : numberKeys.includes(item) ? "number" : "text"}
+                                            placeholder={item}
+                                            onChange={(e) => {
+                                                const newItem = editedItem;
+                                                newItem[item] = e.target.value;
+                                                setEditedItem(newItem);
+                                                onChangeInput(e, editedItem._id);
+                                            }}
+                                        />
+                                    )
                                 ) : (
                                     beneficiary[item]
                                 )}
